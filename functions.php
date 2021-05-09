71ef3883a419fb2a6e28c062d313aa435f254eb0
ohwhatafeeling <pyro_michael@hotmail.com>
2021-05-09 16:35:41 +1000

    Validate input to only accept numeric values

--- functions.php
+++ functions.php
@@ -4,6 +4,9 @@ $num01 = $_GET["num01"];
 $num02 = $_GET["num02"];
 $oper = $_GET["oper"];
 
+$num01 = filter_input(INPUT_GET, 'num01', FILTER_SANITIZE_NUMBER_INT);
+$num02 = filter_input(INPUT_GET, 'num02', FILTER_SANITIZE_NUMBER_INT);
+
 function calculator($num01, $num02, $oper) {
   $sum;
   switch ($oper) {
@@ -20,4 +23,12 @@ function calculator($num01, $num02, $oper) {
   return $sum;
 }
 
-echo "Answer: " . calculator($num01, $num02, $oper);
+if (is_numeric($num01)) {
+  if (is_numeric($num02)) {
+    echo "Answer: " . calculator($num01, $num02, $oper);
+  } else {
+    echo "Invalid entry. Please enter a number only";
+  }
+} else {
+  echo "Invalid entry. Please enter a number only";
+}
